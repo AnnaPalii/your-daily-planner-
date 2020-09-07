ce26df7e6a5d68d5ddea133c53fee52755efcbbd
AnnaPaliils <annapaliy@annas-air.myfiosgateway.com>
2020-09-07 17:39:05 -0400

    working code

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ hourBlock.append(newRow);
 
 // Create column for hours
 var timeEl = document.createElement("div");
-timeEl.setAttribute("class", "col-1");
-timeEl.textContent = workHours[i] % 12;
+timeEl.setAttribute("class", "col-md-1 hour");
+timeEl.textContent = workHours[i] % 13 + parseInt(workHours[i]/13);
 if (i  < 3) {
     timeEl.append(" AM");
 }
@@ -29,22 +29,28 @@ else {
 newRow.append(timeEl);
 
 // Create input row
-var taskDes = document.createElement("input");
-taskDes.setAttribute("id","box "+ workHours[i]);
-taskDes.textContent = "";
+var taskDes = document.createElement("textarea");
+taskDes.setAttribute("id","box "+ workHours[i], "class", "col-md-10 description", "row", "2");
 newRow.append(taskDes);
+
+// check if textarea has data saved in local storage
+var taskTitle = localStorage.getItem("box "+ workHours[i]);
+console.log(taskTitle);
+if (taskTitle !== null) {
+    taskDes.innerText = taskTitle;
+}
 
 // Assign correct color for time slot
 var currentTime = moment().format("HH");
 console.log(currentTime);
 if (currentTime == workHours[i]) {
-    taskDes.setAttribute("class", "present col-6 description");
+    taskDes.setAttribute("class", "present col-6 time-block");
 }
 if (currentTime < workHours[i]) {
-    taskDes.setAttribute("class", "future col-6 description");
+    taskDes.setAttribute("class", "future col-6 time-block");
 }
 if (currentTime > workHours[i]) {
-    taskDes.setAttribute("class", "past col-6 description");
+    taskDes.setAttribute("class", "past col-6 time-block");
 }
 
 // Create save btn
@@ -52,17 +58,16 @@ var saveBtn = document.createElement("button");
 saveBtn.setAttribute("class","saveBtn col-1");
 saveBtn.textContent = "Save";
 newRow.append(saveBtn);
-}
-
-// Function to get data from local storage
-function saveValue (event){
-    event.preventDefault();
-    document.getElementById("saveBtn ").click();
-
-}
-// Function for saving data to local storage
-
-// Function to evaluate hours for prs, current and future
-
 // User clicks button to save description 
+// Function for saving data to local storage
+function saveValue (event){
+    // get value from input box
+    var currentTask = event.currentTarget.previousElementSibling.id;
+    console.log(currentTask);
+    var task = event.currentTarget.previousElementSibling.value;
+    console.log(task);
+    localStorage.setItem(currentTask, task);
+    }
+
 saveBtn.onclick = saveValue;
+}
